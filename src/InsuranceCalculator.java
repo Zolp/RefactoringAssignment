3461709c09dd7c9f1806ff5605645ace6a2a478f
Zolp <aeellery@bsu.edu>
2020-02-13 10:39:06 -0500

    Refactored calculator to properly divide use of function.

--- src/InsuranceCalculator.java
+++ src/InsuranceCalculator.java
@@ -1,3 +1,18 @@
-public class InsuranceCalculator extends InsuranceStrategy {
-
+class InsuranceCalculator extends InsuranceStrategy {
+    double calculateInsurance(double income) {
+        InsuranceStrategy strategy;
+        if (income <= 10000) {
+            strategy = new InsuranceStrategyLow();
+            return strategy.calculateInsuranceVeryHigh(income);
+        } else if (income <= 30000) {
+            strategy = new InsuranceStrategyMedium();
+            return strategy.calculateInsuranceVeryHigh(income);
+        } else if (income <= 60000) {
+            strategy = new InsuranceStrategyHigh();
+            return strategy.calculateInsuranceVeryHigh(income);
+        } else {
+            strategy = new incomeStrategyVeryHigh();
+            return strategy.calculateInsuranceVeryHigh(income);
+        }
+    }
 }
--- src/InsuranceStrategy.java
+++ src/InsuranceStrategy.java
@@ -1,23 +1,5 @@
 public abstract class InsuranceStrategy {
-
-    double calculateInsurance(double income) {
-        InsuranceStrategy strategy;
-        if (income <= 10000) {
-            strategy = new InsuranceStrategyLow();
-            return strategy.calculateInsuranceVeryHigh(income);
-        } else if (income <= 30000) {
-            strategy = new InsuranceStrategyMedium();
-            return strategy.calculateInsuranceVeryHigh(income);
-        } else if (income <= 60000) {
-            strategy = new InsuranceStrategyHigh();
-            return strategy.calculateInsuranceVeryHigh(income);
-        } else {
-            strategy = new incomeStrategyVeryHigh();
-            return strategy.calculateInsuranceVeryHigh(income);
-        }
-    }
-
-    private double calculateInsuranceVeryHigh(double income) {
+    double calculateInsuranceVeryHigh(double income) {
         return (income - getAdjustment()) * getWeight() + getConstant();
     }
 
